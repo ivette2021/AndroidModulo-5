77a8acd0e7fa7c9878baec7bd833568a6fe43c87
ivette2021 <iconstanzav1@gmail.com>
2023-06-21 14:25:24 -0400

    agrege binding en buildgradle

--- app/src/main/java/com/example/modulo5/QuestionFragment.java
+++ app/src/main/java/com/example/modulo5/QuestionFragment.java
@@ -4,14 +4,16 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.modulo5.databinding.FragmentQuestionBinding;
+
 public class QuestionFragment extends Fragment {
 
-
-
+private FragmentQuestionBinding mBinding;
     public QuestionFragment() {
         // Required empty public constructor
     }
@@ -25,6 +27,7 @@ public class QuestionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_question, container, false);
+     mBinding = FragmentQuestionBinding.inflate(inflater, container, false);
+     return mBinding.getRoot();
     }
 }
