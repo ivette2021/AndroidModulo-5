32fea6445c528db874cefd7abb2d848601bb37c6
ivette2021 <iconstanzav1@gmail.com>
2023-06-21 15:16:02 -0400

    uso de fragmentos estaticos

--- app/src/main/java/com/example/modulo5/QuestionFragment.java
+++ app/src/main/java/com/example/modulo5/QuestionFragment.java
@@ -2,12 +2,15 @@ package com.example.modulo5;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.renderscript.ScriptGroup;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import com.example.modulo5.databinding.FragmentQuestionBinding;
 
@@ -29,5 +32,24 @@ private FragmentQuestionBinding mBinding;
         // Inflate the layout for this fragment
      mBinding = FragmentQuestionBinding.inflate(inflater, container, false);
      return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mBinding.radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int i) {
+                int index = mBinding.radiogroup.indexOfChild(mBinding.radiogroup.findViewById(i));
+                switch (index){
+                    case 0:
+                        mBinding.textView1.setText("Si lo conozco");
+                        break;
+                    case 1:
+                        mBinding.textView1.setText("No lo conozco");
+                        break;
+                }
+            }
+        });
     }
 }
