ed5c459d92e8a0f77fb23e14c0e71f8f4e362236
ivette2021 <iconstanzav1@gmail.com>
2023-06-29 02:25:48 -0400

    se añade funcionalidad al primer fragment

--- app/src/main/java/com/example/modulo_5/TitleFragment.java
+++ app/src/main/java/com/example/modulo_5/TitleFragment.java
@@ -3,62 +3,52 @@ package com.example.modulo_5;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TitleFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.modulo_5.databinding.FragmentTitleBinding;
+
 public class TitleFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TitleFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TitleFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TitleFragment newInstance(String param1, String param2) {
-        TitleFragment fragment = new TitleFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+    FragmentTitleBinding binding;
+    public TitleFragment newInstance() {
+        TitleFragment fragment= new  TitleFragment();
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false);
+
+        binding=FragmentTitleBinding.inflate(inflater,container,false);
+        View view  = binding.getRoot();
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!binding.editTextText.getText().toString().isEmpty()){
+                    addLogoTriviaFragment(binding.editTextText.getText().toString());
+                } else{
+                    Toast.makeText(getContext(), "Ingrese su nombre", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        return view;
+    }
+    private  void addLogoTriviaFragment(String name){
+        Bundle bundle  = new Bundle();
+        bundle.putString("clave1", name);
+        LogoTriviaFragment  logoTriviaFragment= LogoTriviaFragment.newInstance(name,"");
+        Navigation.findNavController(binding.getRoot())
+                .navigate(R.id.action_titleFragment_to_logoTriviaFragment,bundle);
     }
 }
