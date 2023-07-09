eae697b66adcf142d52777cd8e11787501485138
ivette2021 <iconstanzav1@gmail.com>
2023-07-09 00:39:02 -0400

    proyecto de vista reciclable y vinculacion con url

---
+++ app/src/main/java/com/example/modulo5_recycleviewimg/datosLista.java
@@ -0,0 +1,17 @@
+package com.example.modulo5_recycleviewimg;
+
+public class datosLista {
+    private String url;
+    private String dato;
+
+    public datosLista(String url,String dato){
+
+        this.url = url;
+        this.dato= dato;
+    }
+
+    public String getUrl(){return  url;}
+    public String getDato(){return  dato;}
+
+}
+
