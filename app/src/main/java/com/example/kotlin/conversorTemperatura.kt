9e83b7cd1f24e7d31d2e0e7fbbce3d491846b4c4
ivette2021 <iconstanzav1@gmail.com>
2023-07-09 18:13:56 -0400

    ejercicio21 corregido

--- app/src/main/java/com/example/kotlin/conversorTemperatura.kt
+++ app/src/main/java/com/example/kotlin/conversorTemperatura.kt
@@ -5,24 +5,27 @@ import java.util.Scanner
 fun main() {
     val scanner = Scanner(System.`in`)
 
-    println("Ingrese una temperatura:")
+    println("Ingrese una temperatura con coma NO PUNTO:")
     val temperatura = scanner.nextDouble()
 
     println("Ingrese la escala de temperatura:")
     println("1. Celsius")
     println("2. Fahrenheit")
     println("3. Kelvin")
+    println("4. todas las escalas")
     val opcion = scanner.nextInt()
 
     when (opcion) {
         1 -> convertirDeCelsius(temperatura)
         2 -> convertirDeFahrenheit(temperatura)
         3 -> convertirDeKelvin(temperatura)
+        4 -> convertirTodo(temperatura)
         else -> println("Opción inválida")
     }
 }
 
-fun convertirDeCelsius(temperatura: Double) {
+fun convertirTodo(temperatura: Double) {
+    val celsius = (temperatura - 32) * (5.0 / 9.0)
     val fahrenheit = (9.0 / 5.0 * temperatura) + 32
     val kelvin = temperatura + 273.15
 
@@ -31,20 +34,22 @@ fun convertirDeCelsius(temperatura: Double) {
     println("Temperatura en Kelvin: ${String.format("%.2f", kelvin)} K")
 }
 
-fun convertirDeFahrenheit(temperatura: Double) {
-    val celsius = (temperatura - 32) * (5.0 / 9.0)
-    val kelvin = (temperatura - 32) * (5.0 / 9.0) + 273.15
+fun convertirDeCelsius(temperatura: Double) {
 
-    println("Temperatura en Fahrenheit: ${String.format("%.2f", temperatura)} °F")
-    println("Temperatura en Celsius: ${String.format("%.2f", celsius)} °C")
-    println("Temperatura en Kelvin: ${String.format("%.2f", kelvin)} K")
+    println("Temperatura en Celsius: ${String.format("%.2f", temperatura)} °C")
+
+}
+
+fun convertirDeFahrenheit(temperatura: Double) {
+
+    val fahrenheit = (9.0 / 5.0 * temperatura) + 32
+    println("Temperatura en Fahrenheit: ${String.format("%.2f", fahrenheit)} °F")
+
 }
 
 fun convertirDeKelvin(temperatura: Double) {
-    val celsius = temperatura - 273.15
-    val fahrenheit = (temperatura - 273.15) * (9.0 / 5.0) + 32
 
-    println("Temperatura en Kelvin: ${String.format("%.2f", temperatura)} K")
-    println("Temperatura en Celsius: ${String.format("%.2f", celsius)} °C")
-    println("Temperatura en Fahrenheit: ${String.format("%.2f", fahrenheit)} °F")
+    val kelvin = (temperatura + 273.15)
+    println("Temperatura en Kelvin: ${String.format("%.2f", kelvin)} K")
+
 }
