b711a8cfd7595c45b1015bff693d20dc554b3c71
ivette2021 <iconstanzav1@gmail.com>
2023-07-11 16:23:47 -0400

    ejercicio 22 parte B

--- app/src/main/java/com/example/kotlin/MainActivity2.kt
+++ app/src/main/java/com/example/kotlin/MainActivity2.kt
@@ -1,8 +1,10 @@
 package com.example.kotlin
+// parte B ejercicio 22 v2
 
+// clase Bid que representa una oferta en una subasta y tiene un atributo amount que indica el importe de la oferta
 data class Bid(val amount: Int)
-fun auctionPrice(bid: Bid?) {
-    val price = bid?.amount ?: 0
+fun auctionPrice(bid: Bid?) { //bid?= anulable
+    val price = bid?.amount ?: 0 // ?:= operador elvis, para asignar un valor de 0 a la variable "price" si "bid" es nulo.
     println("Precio de la subasta: $price")
 
     if (bid == null || bid.amount <= 0) {
@@ -11,10 +13,10 @@ fun auctionPrice(bid: Bid?) {
         println("El ofertante determina el precio del artículo.")
     }
 }
-fun main() {
+fun main() { //se crean dos importes 100 y 0 o nulo
     val bid1 = Bid(100)
     val bid2: Bid? = null
-
+    //se llama a la funcion
     auctionPrice(bid1)
     auctionPrice(bid2)
 }
