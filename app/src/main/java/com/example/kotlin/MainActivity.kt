e0944f59b312d1c63c4eee8380038c8df4575d8c
ivette2021 <iconstanzav1@gmail.com>
2023-07-11 21:24:51 -0400

    ejercicio 23 primera clase Phone.kt

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,21 +1,6 @@
 package com.example.kotlin
  // apartado A del ejercicio 22 v2
 fun main() {
-    //datos que da el ejercicio
-    var num1 = 10
-    var num2 = 33
-    var num3 = 66
 
-    // suma los 3 datos y se almacenan en la varibale suma
-    var suma = num1 + num2 + num3
-    //se ve el resultado en terminal
-    println("La suma es: $suma")
-//como usamos un var se puede sobrescribir la variable segun especificaciones
-    num1 = 55
-    suma = num1 + num2 + num3
-    //aca cambia el resultado suma debido al cambio de num1
-    println("La nueva suma es: $suma")
 
-    var promedio = (num1 + num2 + num3) / 3.0
-    println("El promedio es: $promedio")
 }
---
+++ app/src/main/java/com/example/kotlin/Phone.kt
@@ -0,0 +1,19 @@
+package com.example.kotlin
+fun main() {
+    class Phone(var isScreenLightOn: Boolean = false) {
+        fun switchOn() {
+            isScreenLightOn = true
+            println("encendido")
+        }
+
+        fun switchOff() {
+            isScreenLightOn = false
+            println("apagado")
+        }
+
+        fun checkPhoneScreenLight() {
+            val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+            println("The phone screen's light is $phoneScreenLight.")
+        }
+    }
+}
