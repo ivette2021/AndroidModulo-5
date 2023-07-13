4f3e42f49fcc4168eee8b6e13ef7c7a578165901
ivette2021 <iconstanzav1@gmail.com>
2023-07-13 01:35:59 -0400

    segunda parte logica configurada del landscape

--- app/src/main/java/com/example/bankapp/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -6,23 +6,26 @@ import android.view.View
 import android.widget.EditText
 import android.widget.RadioButton
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    private var btnSaldo: RadioButton?=null
-    private var btnIngresar:RadioButton?=null
-    private var btnRetirar:RadioButton?=null
-    private var btnSalir:RadioButton?=null
+    private var rbSaldo: RadioButton?=null
+    private var rbIngresar:RadioButton?=null
+    private var rbRetirar:RadioButton?=null
+    private var rbSalir:RadioButton?=null
     private var tvsaldo:TextView?=null
     private var txtingresar:EditText?=null
     private var txtretirar:EditText?=null
 
+    private var saldo=500.000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnSaldo =findViewById(R.id.btnSaldo)
-        btnIngresar=findViewById(R.id.btnIngresar)
-        btnRetirar=findViewById(R.id.btnRetirar)
-        btnSalir=findViewById(R.id.confirmButton)
+        rbSaldo =findViewById(R.id.rbSaldo)
+        rbIngresar=findViewById(R.id.rbIngresar)
+        rbRetirar=findViewById(R.id.rbRetirar)
+        rbSalir=findViewById(R.id.confirmButton)
 
         tvsaldo =findViewById(R.id.tvsaldo)
         txtingresar =findViewById(R.id.txtingresar)
@@ -32,20 +35,49 @@ class MainActivity : AppCompatActivity() {
         tvsaldo?.visibility= View.INVISIBLE
         txtingresar?.visibility= View.INVISIBLE
         txtretirar?.visibility= View.INVISIBLE
-        if (btnSaldo?.isChecked()==true){
+        if (rbSaldo?.isChecked()==true){
             tvsaldo?.visibility=View.VISIBLE
-            tvsaldo?.text="Tú Saldo es "
+            tvsaldo?.text="Tú Saldo es $saldo "
         }
-        if (btnIngresar?.isChecked()==true){
+        if (rbIngresar?.isChecked()==true){
             txtingresar?.visibility = View.VISIBLE
         }
-        if (btnRetirar?.isChecked()==true){
+        if (rbRetirar?.isChecked()==true){
             txtretirar?.visibility=View.VISIBLE
         }
-        if (btnSalir?.isChecked()==true){
+        if (rbSalir?.isChecked()==true){
             tvsaldo?.visibility = View.VISIBLE
             tvsaldo?.text="Gracias por preferirnos"
-
         }
     }
-}
+    fun rbConfirmar(view: View){
+        tvsaldo?.visibility= View.INVISIBLE
+        txtingresar?.visibility= View.INVISIBLE
+        txtretirar?.visibility= View.INVISIBLE
+        if (rbSaldo?.isChecked()==true){
+            tvsaldo?.visibility=View.VISIBLE
+            tvsaldo?.text="Tú Saldo es $saldo "
+        }
+        if (rbIngresar?.isChecked()==true){
+            var ingresar=txtingresar?.text.toString().toDouble()
+            saldo=saldo+ingresar
+            tvsaldo?.text="Tú Saldo es $saldo "
+            Toast.makeText(this,"tu operacion se realizo de forma exitos", Toast.LENGTH_LONG).show()
+        }
+        if (rbRetirar?.isChecked()==true){
+            var retirar= txtretirar?.text.toString().toDouble()
+            if(saldo-retirar <0.0){
+                tvsaldo?.text="Saldo Insuficiente"
+        }else{
+            saldo= saldo-retirar
+            tvsaldo?.text="su saldo es de $saldo"
+            Toast.makeText(this,"tu operacion se realizo de forma exitosa",Toast.LENGTH_LONG).show()
+        }
+        if (rbSalir?.isChecked()==true){
+            tvsaldo?.visibility = View.VISIBLE
+            tvsaldo?.text="tu operacion se realizo de forma exitosa"
+            finish()
+            System.exit(0)
+        }
+    }
+}}
