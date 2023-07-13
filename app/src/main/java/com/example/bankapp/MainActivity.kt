2ccf86a4274169b55505fbb697a3ef7ed38903fe
ivette2021 <iconstanzav1@gmail.com>
2023-07-12 23:04:57 -0400

    primera parte logica configurada del landscape

--- app/src/main/java/com/example/bankapp/MainActivity.kt
+++ app/src/main/java/com/example/bankapp/MainActivity.kt
@@ -2,10 +2,50 @@ package com.example.bankapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var btnSaldo: RadioButton?=null
+    private var btnIngresar:RadioButton?=null
+    private var btnRetirar:RadioButton?=null
+    private var btnSalir:RadioButton?=null
+    private var tvsaldo:TextView?=null
+    private var txtingresar:EditText?=null
+    private var txtretirar:EditText?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnSaldo =findViewById(R.id.btnSaldo)
+        btnIngresar=findViewById(R.id.btnIngresar)
+        btnRetirar=findViewById(R.id.btnRetirar)
+        btnSalir=findViewById(R.id.confirmButton)
+
+        tvsaldo =findViewById(R.id.tvsaldo)
+        txtingresar =findViewById(R.id.txtingresar)
+        txtretirar =findViewById(R.id.txtretirar)
+    }
+    fun accion(view: View){
+        tvsaldo?.visibility= View.INVISIBLE
+        txtingresar?.visibility= View.INVISIBLE
+        txtretirar?.visibility= View.INVISIBLE
+        if (btnSaldo?.isChecked()==true){
+            tvsaldo?.visibility=View.VISIBLE
+            tvsaldo?.text="Tú Saldo es "
+        }
+        if (btnIngresar?.isChecked()==true){
+            txtingresar?.visibility = View.VISIBLE
+        }
+        if (btnRetirar?.isChecked()==true){
+            txtretirar?.visibility=View.VISIBLE
+        }
+        if (btnSalir?.isChecked()==true){
+            tvsaldo?.visibility = View.VISIBLE
+            tvsaldo?.text="Gracias por preferirnos"
+
+        }
     }
 }
